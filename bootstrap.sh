8b1dc739d71c7f3653aa2fd952290abf58e747c3
Logan Cox <lmc@ou.edu>
2016-08-05 00:56:58 -0500

    Uncomment role installation.

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,6 +17,6 @@ chmod 600 /etc/ansible/private_key
 chmod -x /etc/ansible/hosts
 
 # run ansible
-#ansible-galaxy install -r /vagrant/requirements.yml --force
+ansible-galaxy install -r /vagrant/requirements.yml --force
 ansible-playbook --inventory-file=/etc/ansible/hosts --private-key=/etc/ansible/private_key /vagrant/vagrant.yml
 
