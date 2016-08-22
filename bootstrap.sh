e28a515e709e3f84302a046934f6518eb918b3af
Chris Cone <chris.cone@ou.edu>
2016-08-22 11:52:51 -0500

    Added copy of files to /tmp/downloads/ to speed up test deployments

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,9 +9,9 @@ yum install -y git
 yum install -y ansible
 
 # ansible complains if these files are on the windows share because permissions
-cp /vagrant/hosts /etc/ansible/hosts
+# cp /vagrant/hosts /etc/ansible/hosts
 cp /vagrant/ansible.cfg /etc/ansible/ansible.cfg
-cp /vagrant/.vagrant/machines/default/virtualbox/private_key /etc/ansible/private_key
+# cp /vagrant/.vagrant/machines/default/virtualbox/private_key /etc/ansible/private_key
 # set complaint-free permissions
 chmod 600 /etc/ansible/private_key
 chmod -x /etc/ansible/hosts
